5327c12e7da942a5a1bdb0e006a9d5c781a5369a
Troy D. Hanson <tdh@tkhanson.net>
2015-10-29 14:59:20 -0400

    nano-epull

--- nanomsg/README.md
+++ nanomsg/README.md
@@ -2,20 +2,11 @@ Examples of using [nanomsg](http://nanomsg.org)
 
 * nano-pull.c: simple PULL receiver
 * nano-push.c: simple PUSH publisher
-* nano-epoll.c: poll nanomsg and OS-level sockets 
+* nano-epull.c: manage nano PULL socket and OS descriptors 
 
 nano-pull binds a local port, and nano-push connects to it.
 Many copies of nano-push can run at the same time. Only one
-nano-pull can be effective at once (have the port bound).
+nano-pull can be effective at once since it binds the port.
 
-OS and nano socket monitoring in one event loop
-
-A program that manages OS file descriptors and nano sockets in
-a single event loop (say, epoll) can use nano's socket option API
-to get an underlying file descriptor.
-
-A real program may also want to handle signals properly in the main loop.
-We can convert signals into file descriptor events using signalfd. Then use
-epoll. Or, we can convert file descriptor events into signals (signal-driven
-I/O). Then use sigwaitinfo.
-
+nano-epull demonstrates usage of epoll with nano and OS 
+descriptors in one event loop. It handles signals properly.
---
+++ nanomsg/nano-epull.c
@@ -0,0 +1,168 @@
+/*
+ * Manage nano socket and OS descriptors via epoll.
+ * signals are also handled signalfd
+ */
+
+#include <errno.h>
+#include <sys/epoll.h>
+#include <sys/signalfd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <nanomsg/nn.h>
+#include <nanomsg/pipeline.h>
+
+struct {
+  int verbose;
+  char *prog;
+  int signal_fd;
+  int epoll_fd;
+  int ticks;
+  int nn_socket; /* nano socket */
+  int nn_fd;     /* nano descriptor */
+  char *local;   /* nano binding */
+  char *buf;     /* nano allocd msg */
+  int len;       /* len of msg */
+} cfg = {
+  .signal_fd = -1,
+  .epoll_fd = -1,
+  .local = "tcp://127.0.0.1:9995",
+  .nn_socket = -1,
+  .nn_fd = -1,
+};
+
+void usage() {
+  fprintf(stderr,"usage: %s [-v] \n", cfg.prog);
+  exit(-1);
+}
+
+/* signals that we'll accept via signalfd in epoll */
+int sigs[] = {SIGHUP,SIGTERM,SIGINT,SIGQUIT,SIGALRM};
+
+void periodic_work() {
+  fprintf(stderr,"periodic work...\n");
+}
+
+int new_epoll(int events, int fd) {
+  int rc;
+  struct epoll_event ev;
+  memset(&ev,0,sizeof(ev)); // placate valgrind
+  ev.events = events;
+  ev.data.fd= fd;
+  if (cfg.verbose) fprintf(stderr,"adding fd %d to epoll\n", fd);
+  rc = epoll_ctl(cfg.epoll_fd, EPOLL_CTL_ADD, fd, &ev);
+  if (rc == -1) {
+    fprintf(stderr,"epoll_ctl: %s\n", strerror(errno));
+  }
+  return rc;
+}
+
+int handle_signal() {
+  int rc=-1;
+  struct signalfd_siginfo info;
+  
+  if (read(cfg.signal_fd, &info, sizeof(info)) != sizeof(info)) {
+    fprintf(stderr,"failed to read signal fd buffer\n");
+    goto done;
+  }
+
+  switch(info.ssi_signo) {
+    case SIGALRM: 
+      if ((++cfg.ticks % 10) == 0) periodic_work();
+      alarm(1); 
+      break;
+    default: 
+      fprintf(stderr,"got signal %d\n", info.ssi_signo);  
+      goto done;
+      break;
+  }
+
+ rc = 0;
+
+ done:
+  return rc;
+}
+
+int handle_nn() {
+  int rc=-1;
+
+  rc = (cfg.len = nn_recv(cfg.nn_socket, &cfg.buf, NN_MSG, 0));
+  if (rc < 0) goto done;
+  fprintf(stderr,"received: %.*s", cfg.len, cfg.buf);
+  nn_freemsg(cfg.buf);
+
+  rc = 0;
+
+ done:
+  if (rc < 0) fprintf(stderr,"nano: %s\n", nn_strerror(errno));
+  return rc;
+}
+
+int main(int argc, char *argv[]) {
+  int opt, rc=0, eid;
+  unsigned n;
+  cfg.prog = argv[0];
+  struct epoll_event ev;
+
+  while ( (opt=getopt(argc,argv,"vh")) != -1) {
+    switch(opt) {
+      case 'v': cfg.verbose++; break;
+      case 'h': default: usage(); break;
+    }
+  }
+
+  /* block all signals. we take signals synchronously via signalfd */
+  sigset_t all;
+  sigfillset(&all);
+  sigprocmask(SIG_SETMASK,&all,NULL);
+
+  /* a few signals we'll accept via our signalfd */
+  sigset_t sw;
+  sigemptyset(&sw);
+  for(n=0; n < sizeof(sigs)/sizeof(*sigs); n++) sigaddset(&sw, sigs[n]);
+
+  /* create the signalfd for receiving signals */
+  cfg.signal_fd = signalfd(-1, &sw, 0);
+  if (cfg.signal_fd == -1) {
+    fprintf(stderr,"signalfd: %s\n", strerror(errno));
+    goto done;
+  }
+
+  /* set up the epoll instance */
+  cfg.epoll_fd = epoll_create(1); 
+  if (cfg.epoll_fd == -1) {
+    fprintf(stderr,"epoll: %s\n", strerror(errno));
+    goto done;
+  }
+
+  /* set up the nano PULL socket */
+  rc = (cfg.nn_socket = nn_socket(AF_SP, NN_PULL));
+  if (rc < 0) goto done;
+  rc = (eid = nn_bind(cfg.nn_socket, cfg.local));
+  if (rc < 0) goto done;
+
+  /* add descriptors of interest */
+  size_t fd_sz = sizeof(int);
+  rc = nn_getsockopt(cfg.nn_socket, NN_SOL_SOCKET, NN_RCVFD, &cfg.nn_fd, &fd_sz);
+  if (rc < 0) goto done;
+  if (new_epoll(EPOLLIN, cfg.nn_fd)) goto done;     // nano socket
+  if (new_epoll(EPOLLIN, cfg.signal_fd)) goto done; // signal socket
+
+  fprintf(stderr,"starting... press ctrl-c to exit\n");
+
+  alarm(1);
+  while (epoll_wait(cfg.epoll_fd, &ev, 1, -1) > 0) {
+    if (cfg.verbose > 1)  fprintf(stderr,"epoll reports fd %d\n", ev.data.fd);
+    if (ev.data.fd == cfg.nn_fd)     { if (handle_nn() < 0) goto done; }
+    if (ev.data.fd == cfg.signal_fd) { if (handle_signal() < 0) goto done; }
+  }
+
+done:
+  if (rc < 0) fprintf(stderr,"nano: %s\n", nn_strerror(errno));
+  if (cfg.nn_socket >= 0) nn_close(cfg.nn_socket);
+  if (cfg.epoll_fd != -1) close(cfg.epoll_fd);
+  if (cfg.signal_fd != -1) close(cfg.signal_fd);
+  return 0;
+}
