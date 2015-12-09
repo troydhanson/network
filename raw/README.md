0e2664488d8823b6d4cc0065f293047c81df7dac
Troy D. Hanson <tdh@tkhanson.net>
2015-12-09 11:16:16 -0500

    rx-fan

--- raw/README.md
+++ raw/README.md
@@ -4,10 +4,13 @@ They can also capture packets.
 
 * rx       -  recvmsg-based capture 
 * rx-dump  -  recvmsg-based capture, writes pcap
+* rx-fan   -  recvmsg-based load-balanced multi-process capture `PACKET_FANOUT`
 * rx-ring1 - `PACKET_RX_RING`-based capture, uses API version `TPACKET_V1`
 * rx-ring2 - `PACKET_RX_RING`-based capture, uses API version `TPACKET_V2`
 * rx-ring3 - `PACKET_RX_RING`-based capture, uses API version `TPACKET_V3` 
 * rx-tx    - recvfrom/sendto frame repeater
+
+`PACKET_RX_RING` notes
 
 The difference between `TPACKET_V1` and `TPACKET_V2` is minimal. There was
 a change to the `tpacket_hdr` structure to use explicit sized types.  Also
@@ -15,11 +18,9 @@ the timestamps changed to have nanosecond resoltuion. In `TPACKET_V3` more
 significant changes was made. The ring slots became variable-width so short
 packets take less space. Polling became block-level instead of packet-level. 
 
-As a matter of taste Ring1/2 are more elegant and they seem to "just work". 
-I find that Ring3 has more unpredictable behavior (once in a while it drops
-a packet for no obvious reason, and does not always wake up the poll). Maybe 
-it is an error in the rx-ring3 program. For my purposes I prefer Ring2.
+As a matter of personal taste Ring1/2 seem more elegant and they "just work". 
+Ring3 seems to be more unpredictable- once in a while it drops a packet for 
+no obvious reason, and does not always wake up the poll. Perhaps it is just
+a bug in rx-ring3.
 
-TODO
-
-* fanout
+`PACKET_FANOUT` may be used with `PACKET_RX_RING`. This is not shown here.
---
+++ raw/rx-fan.c
@@ -0,0 +1,355 @@
+/* 
+ * Use a Linux AF_PACKET socket to read packets, using PACKET_FANOUT
+ * to load balance the packets among multiple processes.
+ * 
+ * see packet(7)
+ *
+ * This is a test program to sanity check the received packet data by dumping 
+ * it out in pcap format so it can be analyzed in tcpdump. 
+ *
+ */
+
+#include <errno.h>
+#include <sys/epoll.h>
+#include <sys/signalfd.h>
+#include <sys/prctl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+struct {
+  int verbose;
+  time_t now;
+  char *prog;
+  char *dev;
+  char *out;
+  int ticks;
+  int nproc;
+  int rx_fd;
+  int signal_fd;
+  int epoll_fd;
+  int out_fd;
+  int id;  /* parent = 0, first child = 1, etc */
+} cfg = {
+  .dev = "eth0",
+  .out = "test.pcapN",
+  .nproc = 2,
+  .rx_fd = -1,
+  .signal_fd = -1,
+  .epoll_fd = -1,
+  .out_fd = -1,
+};
+
+void usage() {
+  fprintf(stderr,"usage: %s [-v] [options]\n"
+                 " options:      -i <eth>        (interface name)\n"
+                 "               -o <file.pcap>  (output file)\n"
+                 "               -n <nproc>      (number processes)\n"
+                 "\n",
+          cfg.prog);
+  exit(-1);
+}
+
+/* signals that we'll accept via signalfd in epoll */
+int sigs[] = {SIGHUP,SIGTERM,SIGINT,SIGQUIT,SIGALRM,SIGCHLD};
+
+int setup_rx(void) {
+  int rc=-1, ec;
+
+  /* any link layer protocol packets (linux/if_ether.h) */
+  int protocol = htons(ETH_P_ALL);
+
+  /* create the packet socket */
+  cfg.rx_fd = socket(AF_PACKET, SOCK_RAW, protocol);
+  if (cfg.rx_fd == -1) {
+    fprintf(stderr,"socket: %s\n", strerror(errno));
+    goto done;
+  }
+
+  /* convert interface name to index (in ifr.ifr_ifindex) */
+  struct ifreq ifr; 
+  strncpy(ifr.ifr_name, cfg.dev, sizeof(ifr.ifr_name));
+  ec = ioctl(cfg.rx_fd, SIOCGIFINDEX, &ifr);
+  if (ec < 0) {
+    fprintf(stderr,"failed to find interface %s\n", cfg.dev);
+    goto done;
+  }
+
+  /* bind to receive the packets from just one interface */
+  struct sockaddr_ll sl;
+  memset(&sl, 0, sizeof(sl));
+  sl.sll_family = AF_PACKET;
+  sl.sll_protocol = protocol;
+  sl.sll_ifindex = ifr.ifr_ifindex;
+  ec = bind(cfg.rx_fd, (struct sockaddr*)&sl, sizeof(sl));
+  if (ec < 0) {
+    fprintf(stderr,"socket: %s\n", strerror(errno));
+    goto done;
+  }
+
+  /* join fanout group. id should be unique in a network namespace. */
+  unsigned fanout_id = 0;
+  unsigned fanout_type = PACKET_FANOUT_LB;
+  unsigned fan = (fanout_id | ( fanout_type << 16U));
+  ec = setsockopt(cfg.rx_fd, SOL_PACKET, PACKET_FANOUT, &fan, sizeof(fan));
+  if (ec < 0) {
+    fprintf(stderr,"setsockopt PACKET_FANOUT: %s\n", strerror(errno));
+    goto done;
+  }
+
+  /* at this point we have a raw packet socket. it could be
+   * used to transmit or receive packets coming on the nic.
+   * we still need to set promiscuous mode to get all packets. */
+  struct packet_mreq m;
+  memset(&m, 0, sizeof(m));
+  m.mr_ifindex = ifr.ifr_ifindex;
+  m.mr_type = PACKET_MR_PROMISC;
+  ec = setsockopt(cfg.rx_fd, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &m, sizeof(m));
+  if (ec < 0) {
+    fprintf(stderr,"setsockopt PACKET_ADD_MEMBERSHIP: %s\n", strerror(errno));
+    goto done;
+  }
+
+  /* enable ancillary data, providing packet length and snaplen, etc */
+  int on = 1;
+  ec = setsockopt(cfg.rx_fd, SOL_PACKET, PACKET_AUXDATA, &on, sizeof(on));
+  if (ec < 0) {
+    fprintf(stderr,"setsockopt PACKET_AUXDATA: %s\n", strerror(errno));
+    goto done;
+  }
+
+  rc = 0;
+
+ done:
+  return rc;
+}
+
+void periodic_work() {
+  cfg.now = time(NULL);
+}
+
+int new_epoll(int events, int fd) {
+  int rc;
+  struct epoll_event ev;
+  memset(&ev,0,sizeof(ev)); // placate valgrind
+  ev.events = events;
+  ev.data.fd= fd;
+  if (cfg.verbose) fprintf(stderr,"adding fd %d to epoll\n", fd);
+  rc = epoll_ctl(cfg.epoll_fd, EPOLL_CTL_ADD, fd, &ev);
+  if (rc == -1) {
+    fprintf(stderr,"epoll_ctl: %s\n", strerror(errno));
+  }
+  return rc;
+}
+
+int handle_signal(void) {
+  int rc=-1;
+  struct signalfd_siginfo info;
+  
+  if (read(cfg.signal_fd, &info, sizeof(info)) != sizeof(info)) {
+    fprintf(stderr,"failed to read signal fd buffer\n");
+    goto done;
+  }
+
+  switch(info.ssi_signo) {
+    case SIGALRM: 
+      cfg.ticks++;
+      periodic_work();
+      alarm(1); 
+      break;
+    default: 
+      fprintf(stderr,"got signal %d\n", info.ssi_signo);  
+      goto done;
+      break;
+  }
+
+ rc = 0;
+
+ done:
+  return rc;
+}
+
+/* dump a single packet. our timestamp has 1sec resolution */
+void dump(char *buf, size_t len) {
+  unsigned len32 = (unsigned)len;
+  unsigned zero = 0;
+  unsigned now = (unsigned)cfg.now;
+
+  write(cfg.out_fd, &now, sizeof(uint32_t));  /* ts_sec */
+  write(cfg.out_fd, &zero, sizeof(uint32_t)); /* ts_usec */
+  write(cfg.out_fd, &len32, sizeof(uint32_t)); /* caplen */
+  write(cfg.out_fd, &len32, sizeof(uint32_t)); /* len */
+
+  write(cfg.out_fd, buf, len); /* packet content */
+}
+
+int handle_packet(void) {
+  int rc=-1;
+  ssize_t nr;
+  struct tpacket_auxdata *pa; /* for PACKET_AUXDATA; see packet(7) */
+  struct cmsghdr *cmsg;
+  struct {
+    struct cmsghdr h;
+    struct tpacket_auxdata a;
+  } u;
+
+  /* we get the packet and metadata via recvmsg */
+  struct msghdr msgh;
+  memset(&msgh, 0, sizeof(msgh));
+
+  /* ancillary data; we requested packet metadata (PACKET_AUXDATA) */
+  msgh.msg_control = &u;
+  msgh.msg_controllen = sizeof(u);
+
+  /* buffer for packet content itself. */
+  char buf[8000];
+  struct iovec iov;
+  iov.iov_base = buf;
+  iov.iov_len = sizeof(buf);
+  msgh.msg_iov = &iov;
+  msgh.msg_iovlen = 1;
+
+  nr = recvmsg(cfg.rx_fd, &msgh, 0);
+  if (nr <= 0) {
+    fprintf(stderr,"[%d] recvmsg: %s\n", cfg.id, nr ? strerror(errno) : "eof");
+    goto done;
+  }
+
+  fprintf(stderr,"[%d] received %lu bytes of message data\n", cfg.id, (long)nr);
+  fprintf(stderr,"[%d] received %lu bytes of control data\n", cfg.id, (long)msgh.msg_controllen);
+  cmsg = CMSG_FIRSTHDR(&msgh);
+  if (cmsg == NULL) {
+    fprintf(stderr,"[%d] ancillary data missing from packet\n", cfg.id);
+    goto done;
+  }
+  pa = (struct tpacket_auxdata*)CMSG_DATA(cmsg);
+  fprintf(stderr, " [%d] packet length  %u\n", cfg.id, pa->tp_len);
+  fprintf(stderr, " [%d] packet snaplen %u\n", cfg.id, pa->tp_snaplen);
+
+  dump(buf,nr);
+
+  rc = 0;
+
+ done:
+  return rc;
+}
+
+const uint8_t pcap_glb_hdr[] = {
+ 0xd4, 0xc3, 0xb2, 0xa1,  /* magic number */
+ 0x02, 0x00, 0x04, 0x00,  /* version major, version minor */
+ 0x00, 0x00, 0x00, 0x00,  /* this zone */
+ 0x00, 0x00, 0x00, 0x00,  /* sigfigs  */
+ 0xff, 0xff, 0x00, 0x00,  /* snaplen  */
+ 0x01, 0x00, 0x00, 0x00   /* network  */
+};
+
+int main(int argc, char *argv[]) {
+  struct epoll_event ev;
+  cfg.prog = argv[0];
+  int n,opt,fc;
+  cfg.now=time(NULL);
+
+  while ( (opt=getopt(argc,argv,"vi:o:n:h")) != -1) {
+    switch(opt) {
+      case 'v': cfg.verbose++; break;
+      case 'i': cfg.dev=strdup(optarg); break; 
+      case 'o': cfg.out=strdup(optarg); break; 
+      case 'n': cfg.nproc=atoi(optarg); break; 
+      case 'h': default: usage(); break;
+    }
+  }
+
+  /* block all signals. we take signals synchronously via signalfd */
+  sigset_t all;
+  sigfillset(&all);
+  sigprocmask(SIG_SETMASK,&all,NULL);
+
+  for(n=1; n <= cfg.nproc; n++) {
+    fc = fork();
+    if (fc < 0) {
+      fprintf(stderr,"fork: %s\n", strerror(errno));
+      goto done;
+    } 
+    if (fc == 0) {
+      cfg.id = n;
+      if (prctl(PR_SET_PDEATHSIG, SIGHUP) < 0) {
+        fprintf(stderr,"prctl: %s\n", strerror(errno));
+        goto done;
+      }
+      /* replace last character of outfile with our id */
+      cfg.out = strdup(cfg.out);
+      size_t outlen = strlen(cfg.out);
+      cfg.out[outlen - 1] = '0' + n;
+      fprintf(stderr,"[%d] opened %s\n", cfg.id, cfg.out);
+      /* child break from fork loop */
+      break;
+    }
+  }
+
+  /* a few signals we'll accept via our signalfd */
+  sigset_t sw;
+  sigemptyset(&sw);
+  for(n=0; n < sizeof(sigs)/sizeof(*sigs); n++) sigaddset(&sw, sigs[n]);
+
+  /* create the signalfd for receiving signals */
+  cfg.signal_fd = signalfd(-1, &sw, 0);
+  if (cfg.signal_fd == -1) {
+    fprintf(stderr,"signalfd: %s\n", strerror(errno));
+    goto done;
+  }
+
+  /* set up the epoll instance */
+  cfg.epoll_fd = epoll_create(1); 
+  if (cfg.epoll_fd == -1) {
+    fprintf(stderr,"epoll: %s\n", strerror(errno));
+    goto done;
+  }
+
+  /* add descriptors of interest */
+  if (new_epoll(EPOLLIN, cfg.signal_fd)) goto done; // signals
+
+  /* set up the raw socket */
+  if (cfg.id > 0) {
+    if (setup_rx() < 0) goto done;
+    if (new_epoll(EPOLLIN, cfg.rx_fd)) goto done;     // packets
+    cfg.out_fd = open(cfg.out,O_TRUNC|O_CREAT|O_WRONLY, 0644);
+    if (cfg.out_fd < 0) {
+      fprintf(stderr,"open: %s\n", strerror(errno));
+      goto done;
+    }
+    write(cfg.out_fd, pcap_glb_hdr, sizeof(pcap_glb_hdr));
+  }
+
+  alarm(1);
+
+  while (epoll_wait(cfg.epoll_fd, &ev, 1, -1) > 0) {
+    if (cfg.verbose > 1)  fprintf(stderr,"epoll reports fd %d\n", ev.data.fd);
+    if      (ev.data.fd == cfg.signal_fd)   { if (handle_signal() < 0) goto done; }
+    else if (ev.data.fd == cfg.rx_fd)       { if (handle_packet() < 0) goto done; }
+  }
+
+done:
+  if (cfg.rx_fd != -1) close(cfg.rx_fd);
+  if (cfg.out_fd != -1) {
+    fprintf(stderr,"wrote %s\n", cfg.out);
+    close(cfg.out_fd);
+  }
+  if (cfg.signal_fd != -1) close(cfg.signal_fd);
+  if (cfg.epoll_fd != -1) close(cfg.epoll_fd);
+  if (cfg.id == 0) { for(n=0; n < cfg.nproc; n++) waitpid(-1, NULL, WNOHANG); }
+  return 0;
+}
